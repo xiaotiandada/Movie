46ec3123ce764e32585351fae0fb8f68b45b277b
xiaotian <952822399@qq.com>
2017-12-01 22:17:26 +0800

    nodejs

--- routes/detail.js
+++ routes/detail.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* Detail page. */
-router.get('/movie/1', function(req, res, next) {
+router.get('/movie/:id', function(req, res, next) {
   res.render('detail', {
      title: '详情页面',
      movie:{
